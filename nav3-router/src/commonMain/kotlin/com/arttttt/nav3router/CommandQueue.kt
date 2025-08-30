e05ad305b19cdc0154f27513d6c5d557a32b47e3
arttttt <artem-bambalov@yandex.ru>
2025-08-30 11:58:45 +0300

    correct scope for command queue

--- nav3-router/src/commonMain/kotlin/com/arttttt/nav3router/CommandQueue.kt
+++ nav3-router/src/commonMain/kotlin/com/arttttt/nav3router/CommandQueue.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.launch
 class CommandQueue : NavigatorHolder {
     private var navigator: Navigator? = null
     private val pendingCommands = mutableListOf<Array<out Command>>()
-    private val mainScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+    private val mainScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
 
     override fun setNavigator(navigator: Navigator) {
         this.navigator = navigator
